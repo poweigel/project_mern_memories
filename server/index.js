ebd330781e0c8807ab3f1078434f2f58fc37b7c9
poweigel <30331209+poweigel@users.noreply.github.com>
2021-03-02 11:04:13 -0800

    Rebuilt the Zeet projects.

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb://root:123123@poweigel-mongo-production/admin?authSource=admin&w=1';
+const CONNECTION_URL = 'mongodb://root:123123@poweigel-memories-mongo-production/admin?authSource=admin&w=1';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
